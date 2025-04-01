2e2abfcfefa3266f25056e89761e9617ddbdfd15
cagb2025 <coleenconte@icloud.com>
2025-04-01 11:00:29 +0200

    ajout des classes Praticien Prestation et propose ainsi que leur DAO excepté la classe Propose

---
+++ modele/DAO/PrestationDAO.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace modele\DAO;
+
+use modele\DAO\base\Database;
+use modele\Prestation;
+use PDO;
+
+/** 
+*	PrestationDAO
+*	Implémente l'ensemble des traitements en données pour les utilisateurs.
+*	Associé à la logique métier de la classe Prestation (modele/Prestation.php).
+*/
+
+class PrestationDAO extends Database {
+
+	/** 
+	*	Deux paramètres pour le constructeur du DAO :
+	*	1/ nom de la table
+	*	2/ nom de la clé primaire
+	*	Voir les méthodes du CRUD dans le DAO (modele/DAO/base/Database.php).
+	*/
+
+	public function __construct() {
+		//-------------------------------------------
+		$tableName = 'Prestation';
+		$primaryKey = 'idPresta';
+		//-------------------------------------------
+		parent::__construct($tableName, $primaryKey);
+	}
+	
+	/** 
+	*	Besoins en données issues du métier Prestation (modele/Prestation.php)
+	*	@param object:metier Instance de l'objet métier
+	*	@return array
+	*/
+	private function getAllData($metier): array {
+		return [
+			'libelle' => $metier->getLibelle(),
+			
+			
+		];
+	}
+
+	/** 
+	*	CRUD : create
+	*	@param object:metier Instance de l'objet métier
+	*	@return bool
+	*/
+	public function create($metier): bool {
+		$data = $this->getAllData($metier);
+		//createOne() et getLastKey() sont des méthodes du DAO (modele/DAO/base/Database.php)
+		$bool = $this->createOne($data);
+		$metier->setId( $this->getLastKey() );
+		return $bool;
+	}
+
+	/** 
+	*	CRUD : read
+	*	@param integer Numéro de la clé primaire
+	*	@return mixed object|string|bool
+	*/
+	public function read(int $idPresta=0): mixed {
+		$row = false;
+		if($idPresta>0)$row = $this->getOne($idPresta); //on récupère la ligne/tuple concernée
+		//gestion de l'index en cas d'erreur :
+		if(!$row) {
+			die( __CLASS__ . "->read() : l'index fourni (<b>$id</b>) est invalide !" );
+		}
+		$rowData = (array)$row; //conversion objet --> array
+		unset($rowData[$this->primaryKey], $row); //retire la clé primaire du tableau et $row qui ne sert plus
+		$metier = new Prestation(...$rowData); //crée l'objet Prestation(->Prestation.php) avec toutes les clés du tableau $rowData
+		$metier->setIdPresta($idPresta); //ajoute $id dans l'objet métier (Prestation)
+		return $metier; //retourne l'objet crée
+	}
+	
+	/** 
+	*	CRUD : update
+	*	@param object:metier Instance de l'objet métier
+	*	@return bool
+	*/
+	public function update($metier): bool {
+		$data = $this->getAllData($metier);
+		//updateOne() est une méthode du DAO (modele/DAO/base/Database.php)
+		return $this->updateOne($metier->getIdPresta(), $data);
+	}
+	
+	/** 
+	*	CRUD : delete
+	*	@param object:metier Instance de l'objet métier
+	*	@return bool
+	*/
+	public function delete($metier): bool {
+		//deleteOne() est une méthode du DAO (modele/DAO/base/Database.php)
+		return $this->deleteOne( $metier->getIdPresta() );
+	}
+
+	/**
+	*	Méthode permettant l'accès aux données filtrées pour une recherche du prénom ou du nom, 
+	*	avec une requête préparée.
+	* 	@param string $libelle Nom ou prénom de l'utilisateur
+	* 	@return array
+	*/
+	
+
+	/**
+	*	Méthode sendSQL() implémentée dans le DAO (modele/DAO/base/Database.php)
+	*	Prend en compte la commande SQL et son filtre issue du prepared statement [?]
+	*	Le filtre (ici $libelle) est obligatoirement un tableau !
+	* 	@param string $libelle Prénom de l'utilisateur
+	* 	@return object
+	*/
+	public function getLibelle(string $libelle): \stdClass {
+		//sendSQL() est une méthode du DAO (modele/DAO/base/Database.php)
+		return $this->sendSQL("SELECT * from `" . $this->tableName . "` WHERE libelle = ?", [$libelle]);
+	}
+	
+	/**
+	* Utils infos
+	*/
+	
+	public function getTableName(): string {
+		return $this->tableName;
+	}
+	
+	public function getPrimaryKey(): string {
+		return $this->primaryKey;
+	}
+	
+}
